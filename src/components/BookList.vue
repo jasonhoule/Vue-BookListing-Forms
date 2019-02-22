fd371fbc5ce31234f6d8ff4c9e1b66185bf78c33
Jason Houle <jhoule@connecticutchildrens.org>
2019-02-22 11:27:31 -0500

    module 3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,38 +4,59 @@
     <ul>
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
+    <hr>
+    <h2>Filtered Books By Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters">{{ filter }}</option>
+    </select>
+    <ul>
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
+    </ul>
     <br><hr>
     <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
-import BookItem from "./BookItem";
-import BookForm from "./BookForm";
+  import _ from "lodash";
+  import BookItem from "./BookItem";
+  import BookForm from "./BookForm";
 
-export default {
-  name: "BookList",
-  data() {
-    return {
-      title: "All Books",
-      states: ["Want to Read", "Read", "Reading"],
-      books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true },
-        { title: "American Gods", author: "Neil Gaiman", finishedReading: false },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true }
-      ]
-    };
-  },
-  components: {
-    BookItem,
-    BookForm
-  },
-  methods: {
-    appendBook(bookData) {
-      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finishedReading: bookData.finishedReading });
+  export default {
+    name: "BookList",
+    data() {
+      return {
+        title: "All Books",
+        states: ["Want to Read", "Read", "Reading"],
+        filters: ["bought", "borrowed"],
+        holding: "bought",
+        books: [
+          { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true, ownership: "borrowed" },
+          { title: "American Gods", author: "Neil Gaiman", finishedReading: false, ownership: "bought" },
+          { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true, ownership: "borrowed" }
+        ]
+      };
+    },
+    computed: {
+      filteredBooks() {
+        return _.filter(this.books, ["ownership", this.holding]);
+      }
+    },
+    components: {
+      BookItem,
+      BookForm
+    },
+    methods: {
+      appendBook(bookData) {
+        this.books.push({
+          title: bookData.bookTitle,
+          author: bookData.bookAuthor,
+          finishedReading: bookData.finishedReading,
+          ownership: bookData.ownership
+        });
+      }
     }
-  }
-};
+  };
 </script>
 
 <style>
